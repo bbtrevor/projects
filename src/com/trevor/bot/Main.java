46b12e1e13c691cbad203680737bea068ca88615
bbtrevor <trevorbagbey@gmail.com>
2021-04-29 22:59:43 -0400

    initial commit

---
+++ src/com/trevor/bot/Main.java
@@ -0,0 +1,15 @@
+
+package com.trevor.bot;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javax.sound.sampled.AudioInputStream;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("hello");
+        
+    }
+    
+}
